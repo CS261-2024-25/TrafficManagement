92216cbb00c2958607713d7275459ee5aa19f56b
Alex Do <alexxdo2004@gmail.com>
2025-02-21 17:51:02 +0000

    feat: change junction to specifically cardinal junction

---
+++ Assets/Scripts/Backend/JunctionController/CardinalJunction.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Assets.Scripts.Util;
+
+namespace Assets.Scripts.Backend.JunctionController
+{
+    public class CardinalJunction
+    {
+        
+    }
+}
