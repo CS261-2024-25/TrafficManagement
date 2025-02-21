29a4d31172ebbecd5ad98a5dd371388f96f26bba
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2025-02-21 12:15:17 +0000

    feat(general): test comment

--- Assets/Scripts/Frontend/ExitButton.cs
+++ Assets/Scripts/Frontend/ExitButton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ExitButton : MonoBehaviour // This script is used by Exit manager so multiple quit buttons can be used
 {
-    public void Exit(){
+    public void Exit(){ // Test change
         //Debug.Log("Quit"); // Uncomment for testing
         Application.Quit();
     }
