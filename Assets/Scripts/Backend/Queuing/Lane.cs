978e45cf6a4d73563142a11a5df174eaf04e934b
Alex Do <alexxdo2004@gmail.com>
2025-02-21 11:53:02 +0000

    feat(lane): implement vehicle enter and exit with queue length monitoring
    chore: add constructor for lanes
    
    style: change lane to be specifically an "outgoing" lane
    
    style: lanes have 2 ends
    
    style: realter outgoing to just lane

--- Assets/Scripts/Backend/Queuing/Lane.cs
+++ Assets/Scripts/Backend/Queuing/Lane.cs
@@ -1,10 +1,18 @@
 namespace Assets.Scripts.Backend.Queuing {
+    using System;
     using System.Collections.Generic;
     using Assets.Scripts.Backend.Vehicle;
 
     class Lane 
     {
         private Queue<Vehicle> VehicleQueue;
+        public double PeakQueueLength { get; private set; }
+
+        public Lane()
+        {
+            VehicleQueue = new Queue<Vehicle>();
+            PeakQueueLength = 0;
+        }
 
         public double GetQueueLength()
         {
@@ -18,6 +26,30 @@ namespace Assets.Scripts.Backend.Queuing {
             return total;
         }
 
+        /// <summary>
+        /// Returns current queue length after adding vehicle
+        /// </summary>
+        public double VehicleEnter(Vehicle vehicle)
+        {
+            VehicleQueue.Enqueue(vehicle);
 
+            var currQueueLength = GetQueueLength();
+            PeakQueueLength = Math.Max(PeakQueueLength, currQueueLength);
+
+            return currQueueLength;
+        }
+
+        public (Vehicle?, double) VehicleExit()
+        {
+            if (VehicleQueue.Count > 0)
+            {
+                var leavingVehicle = VehicleQueue.Dequeue();
+                return (leavingVehicle, GetQueueLength());
+            }
+            else
+            {
+                return (null, GetQueueLength());
+            }
+        }
     }
 }
