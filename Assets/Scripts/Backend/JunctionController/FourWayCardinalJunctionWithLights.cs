98f1775a3f782a4ae13cc2030e5be99015745a16
Alex Do <alexxdo2004@gmail.com>
2025-02-25 15:28:49 +0000

    temporary commit, squash later

---
+++ Assets/Scripts/Backend/JunctionController/FourWayCardinalJunctionWithLights.cs
@@ -0,0 +1,18 @@
+using Assets.Scripts.Backend.Junction;
+using Assets.Scripts.Util;
+
+namespace Assets.Scripts.Backend.JunctionController
+{
+    public class FourWayCardinalJunctionWithLights
+    {
+        // private CardinalJunction Junction;
+        // private Engine.Engine Engine;
+        // private CardinalDirection[] Priority;
+
+        // public FourWayCardinalJunctionWithLights(
+        //     Engine.Engine engine,
+        //     CardinalJunction junction,
+
+        // )
+    }
+}
---
+++ Assets/Scripts/Backend/UserInputHandling/UserInputHandling.cs
@@ -0,0 +1,13 @@
+using Assets.Scripts.Backend.JunctionController;
+using Assets.Scripts.Util;
+
+namespace Assets.Scripts.Backend.UserInputHandling
+{
+    public class UserInputHandling
+    {
+        public static FourWayCardinalJunctionWithLights ToFourWayCardinalJunctionWithLights(InputParameters inputParameters)
+        {
+            
+        }
+    }
+}
