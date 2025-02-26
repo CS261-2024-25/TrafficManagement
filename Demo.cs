46f5ff1f1b4289ccca8f82b6ce854aa03a0b670d
adegbite1548 <157822073+adegbite1548@users.noreply.github.com>
2025-02-26 17:25:54 +0000

    Add files via upload

--- Demo.cs
+++ Demo.cs
@@ -10,7 +10,10 @@ public class Demo
     private DateTime startTime;
 
     // Traffic direction configurations
-    private List<DirectionConfig> directions;
+    private DirectionConfig[] directions;
+
+    //Random
+    private Random rnum = new Random();
     
 
 
@@ -50,59 +53,55 @@ public class Demo
     {
         startTime = DateTime.Now;
         //Not finalised
-        directions = new List<DirectionConfig>
+        directions = new DirectionConfig[]
         {
             new DirectionConfig
             {
                 Name = "Northbound",
                 IncomingVph = 300,
-                road = new Road{numLanes = 3},
+                road = new Road{numLanes = 3, RoadLength = 40f},
                 TrafficPriority = 1,
                 HasLeftTurnLane = true,
-                HasBusCycleLane = true,
-                BusCycleVph = 50,
                 ExitNorth = 200,
                 ExitEast = 50,
-                ExitWest = 50
+                ExitWest = 50,
+                ExitSouth = 0
             },
             new DirectionConfig
             {
                 Name = "Southbound",
                 IncomingVph = 250,
-                road = new Road{numLanes = 3},
+                road = new Road{numLanes = 3, RoadLength = 35f},
                 TrafficPriority = 2,
                 HasLeftTurnLane = true,
-                HasBusCycleLane = true,
-                BusCycleVph = 30,
                 ExitSouth = 150,
                 ExitEast = 50,
-                ExitWest = 50
+                ExitWest = 50,
+                ExitNorth = 0
             },
             new DirectionConfig
             {
                 Name = "Eastbound",
                 IncomingVph = 150,
-                road = new Road{numLanes = 3},
+                road = new Road{numLanes = 3, RoadLength = 50f},
                 TrafficPriority = 2,
                 HasLeftTurnLane = true,
-                HasBusCycleLane = true,
-                BusCycleVph = 10,
                 ExitNorth = 50,
                 ExitEast = 50,
-                ExitSouth = 50
+                ExitSouth = 50,
+                ExitWest = 0
             },
             new DirectionConfig
             {
                 Name = "Westbound",
                 IncomingVph = 100,
-                road = new Road{numLanes = 3},
+                road = new Road{numLanes = 3, RoadLength = 30f},
                 TrafficPriority = 4,
                 HasLeftTurnLane = true,
-                HasBusCycleLane = true,
-                BusCycleVph = 25,
                 ExitNorth = 25,
                 ExitSouth = 25,
-                ExitWest = 50
+                ExitWest = 50,
+                ExitEast = 0
             }
 
         };
@@ -124,7 +123,7 @@ public class Demo
         }
 
         if(currentTime>=phaseEndTime){
-            currPhaseIndex = (currPhaseIndex + 1) % directions.Count; // switches to next phase 
+            currPhaseIndex = (currPhaseIndex + 1) % directions.Length; // switches to next phase 
 
             phaseEndTime = currentTime.AddSeconds(calculatePhaseDuration(directions[currPhaseIndex].TrafficPriority));
         }
@@ -139,6 +138,37 @@ public class Demo
         return 30f +(priority * 5f); // as the priority increases , the phase duration increases by 5 starting from 30 (priority 0)
     }
 
+    private string DetermineExitDirection(DirectionConfig dir)
+    {
+        var exits = new Dictionary<string, int> // gets each exit direction's vph
+        { 
+            {"North", dir.ExitNorth},
+            {"South", dir.ExitSouth},
+            {"East", dir.ExitEast},
+            {"West", dir.ExitWest}
+        };
+
+        int total = exits.Values.Sum();
+        int random = rnum.Next(0, total-1);
+        int cumulative = 0;
+        
+        /*  
+            Note: The idea here is that each exit direction has its own range on a graph and if the random number falls within its range then it is selected ,
+            E.g N = 0 , S = 150 , E = 50 , W = 50 with random = 150. Graph range -> N = none , S = 0 - 149, E = 150 - 199 , W = 200 - 249
+            150 !< 0,
+            150 !< 150,
+            150 < 200 
+            So East is picked.
+        */
+        foreach (var exit in exits)
+        {
+            cumulative += exit.Value;
+            if (random < cumulative)
+                return exit.Key;
+        }
+        return ""; 
+    }
+
     
 
     private void RunSimulation()
@@ -148,20 +178,49 @@ public class Demo
 
         while(startTime < endTime) {
 
+            
+            //Enqueue vehicles
             foreach (var dir in directions){
-                
-                double Vps = dir.IncomingVph/3600;
-                Random rnum = new Random();
+
+                double Vps = dir.IncomingVph/3600; // convert to vheicles per second as we only queue in one car at a time
+
                 if(rnum.NextDouble() < Vps){
-                    Vehicle car = new Vehicle();
-                    car.EntryTime = currentTime;
-                    int car_lane = rnum.Next(0,dir.road.numLanes-1);
-                    dir.road.Lanes[car_lane].Enqueue(car);
+
+                    Vehicle car = new Vehicle {EntryTime = currentTime, vehicleLength = rnum.NextDouble() * ((dir.road.RoadLength/10)-1) + 1};// creates car object and randomly generates a vehicleLength for it (logic for length can be chnaged)
+                    int car_lane = rnum.Next(0,dir.road.numLanes-1); // randomly picks a lane to enqueue a car to.
+                    Road _road = dir.road; 
+
+                    if(_road.RoadLength <= _road.Lanes[car_lane].currLaneLength + car.vehicleLength){ // checks if when a new vehicle is added , it exceeds the length of the road.
+
+                        _road.Lanes[car_lane].currLaneLength += car.vehicleLength; // updates the current length
+                        _road.Lanes[car_lane].vehicleQueue.Enqueue(car); // adds car to queue
+                        
+                    }
                 }
             }
 
+            //update the traffic lights
             processTrafficLights(currentTime);
+
             
+            DirectionConfig currDir = directions[currPhaseIndex];
+            string Exit = DetermineExitDirection(currDir);
+            int curr_lane = rnum.Next(0,currDir.road.numLanes-1);
+            Queue<Vehicle> vehicleQueue = currDir.road.Lanes[curr_lane].vehicleQueue;
+
+
+
+            //If block used to overrite the exit in the case that we have a left turn lane and we are working with the 1st lane.
+            if(currDir.HasLeftTurnLane && curr_lane == 0){
+                    Exit = currDir.LeftTurnDir;
+            }
+
+            if(vehicleQueue.Count == 0){
+                currentTime = currentTime.AddSeconds(1);
+                continue;
+            }
+
+            Vehicle currVehicle = vehicleQueue.Dequeue();
             
 
         }
@@ -181,7 +240,6 @@ public class Demo
         public int TrafficPriority { get; set; }  // 0-4 priority 
 
         public bool HasLeftTurnLane {get; set;}
-        public bool HasBusCycleLane {get; set;}
         public int BusCycleVph {get; set;}
         
         // Vehicles exiting in different directions
@@ -189,13 +247,37 @@ public class Demo
         public int ExitEast { get; set; }   
         public int ExitWest { get; set; }
         public int ExitSouth {get; set; }   
+
+        private string _LeftTurnDir {get; set;}
+
+        public string LeftTurnDir {
+            get{return _LeftTurnDir;}
+            set{
+                switch(Name){
+                    case "Northbound":
+                        _LeftTurnDir = "West";
+                        break;
+                    case "Eastbound":
+                        _LeftTurnDir = "North";
+                        break;
+                    case "Westbound":
+                        _LeftTurnDir = "South";
+                        break;
+                    default : 
+                        _LeftTurnDir = "East";
+                        break;
+                }
+            }
+        }
     }
 
-    //change
+    
     public class Road{
 
         private int number_lanes {get; set;} // Number of lanes
-        public Dictionary<int,Queue<Vehicle>> Lanes= new Dictionary<int,Queue<Vehicle>>() ;
+        public Dictionary<int,VehicleLane> Lanes= new Dictionary<int,VehicleLane>() ;
+
+        public double RoadLength {get; set;}
 
         public int numLanes {
             get{return number_lanes;}
@@ -209,12 +291,14 @@ public class Demo
             }
         }
 
+    
+
         
 
         private void InitLanes(){
             Lanes.Clear();
             for(int i = 0; i < number_lanes; i++){
-                Lanes.Add(i,new Queue<Vehicle>());
+                Lanes.Add(i,new VehicleLane{currLaneLength = 0});
             }
 
         }
@@ -223,13 +307,21 @@ public class Demo
         
     }
 
+    public class VehicleLane {
+
+        public double currLaneLength {get; set;}
+        public Queue<Vehicle> vehicleQueue = new Queue<Vehicle>();
+    }
+
     
 
     //Vehicle structure containing exit and entry time to calculate wait time.
     public class Vehicle
     {
+        public double vehicleLength {get; set;}
         public DateTime EntryTime { get; set; }
         public DateTime ExitTime { get; set; }
+        public string ExitDir {get; set;}
     }
 
 
