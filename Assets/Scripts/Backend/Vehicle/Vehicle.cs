171cf48feeb4a47a222de7c1aa85a597c6bc0bda
adegbite1548 <157822073+adegbite1548@users.noreply.github.com>
2025-03-03 11:56:21 +0000

    changed vehicle.cs and added simulation folder

--- Assets/Scripts/Backend/Vehicle/Vehicle.cs
+++ Assets/Scripts/Backend/Vehicle/Vehicle.cs
@@ -1,5 +1,7 @@
 namespace Assets.Scripts.Backend.Vehicle 
 {
+    using System;
+    using System.Collections.Generic;
     using Assets.Scripts.Backend.Engine;
     abstract public class Vehicle 
     {
@@ -15,5 +17,22 @@ namespace Assets.Scripts.Backend.Vehicle
             CreatedAt = engine.SimulationTime;
             VehicleId = vehicleId;
         }
+
+        //Dictionary of all possible vehicles
+        public static readonly Dictionary<string, (double length, double spaceBehind)> VehicleTypes =
+            new()
+            {
+                { "Car", (4.5, 1.5) },
+                { "Truck", (10.0, 3.0) },
+                { "Bus", (12.0, 3.5) },
+                { "Motorcycle", (2.0, 1.0) }
+            };
+    }
+
+    //Generic vehicle class so we can make use of the abstract vehicle class
+    public class GenericVehicle : Vehicle
+    {
+        public GenericVehicle(Engine engine, double vehicleLength, double minSpaceBehind, uint vehicleId)
+            : base(engine, vehicleLength, minSpaceBehind, vehicleId) { }
     }
 }
