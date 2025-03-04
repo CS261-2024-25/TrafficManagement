625705b4146a74f5a677d1fc13301b18d20ff145
TroubleBubble1 <arushi.khemani@gmail.com>
2025-03-04 01:18:58 +0000

    fix(added back button after deletion)

--- Assets/Scripts/Frontend/Gameobject Configs/BackButton.cs
+++ Assets/Scripts/Frontend/Gameobject Configs/BackButton.cs
@@ -6,4 +6,9 @@ public class BackButton : MonoBehaviour
     public void BackToParameter(){
         SceneManager.LoadScene("ParameterSelectScreen");
     }
+
+    //Method to go back to  config screen for Traffic flow  FROM results screen 
+    public void BackToTrafficFlowScreen(){
+        SceneManager.LoadScene("TrafficFlowSelect");
+    }
 }
