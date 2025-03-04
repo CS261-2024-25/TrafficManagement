9928c911e31f666d5923b1e0a958e8fd77aed368
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2025-03-04 12:14:09 +0000

    fix(UI): deleted duplicate files

--- Assets/ExitButton.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ExitButton : MonoBehaviour // This script is used by Exit manager so multiple quit buttons can be used
-{
-    public void Exit(){
-        //Debug.Log("Quit"); // Uncomment for testing
-        Application.Quit();
-    }
-}
