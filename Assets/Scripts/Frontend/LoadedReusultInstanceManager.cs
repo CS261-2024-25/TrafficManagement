f33eff687f5929f9260f3ea05a518129d98b89c1
TroubleBubble1 <arushi.khemani@gmail.com>
2025-03-06 13:50:39 +0000

    feat(separated LoadedReusultInstanceManager script)

---
+++ Assets/Scripts/Frontend/LoadedReusultInstanceManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+//Script for donot destroy game object script for  persistent junction data 
+
+public class LoadedReusultInstanceManager : MonoBehaviour
+{
+    public static LoadedReusultInstanceManager Instance; 
+
+    public ResultJunctionEntrance northResult; 
+    public ResultJunctionEntrance southResult; 
+    public ResultJunctionEntrance eastResult; 
+    public ResultJunctionEntrance westResult; 
+
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
