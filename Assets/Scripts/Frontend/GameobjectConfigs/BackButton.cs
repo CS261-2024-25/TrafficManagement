eb3a1081a81099acf5aa4b9a926d1ebe5b922198
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2025-03-08 12:12:25 +0000

    fix(graphic sim): fixed back button

--- Assets/Scripts/Frontend/GameobjectConfigs/BackButton.cs
+++ Assets/Scripts/Frontend/GameobjectConfigs/BackButton.cs
@@ -7,13 +7,18 @@ public class BackButton : MonoBehaviour
         SceneManager.LoadScene("ParameterSelectScreen");
     }
 
-    //Method to go back to  config screen for Traffic flow  FROM results screen 
+    //Method to go back to config screen for Traffic flow FROM results screen 
     public void BackToTrafficFlowScreen(){
         SceneManager.LoadScene("TrafficFlowSelect");
     }
 
-    //Method to go back to  config screen for Traffic flow  FROM results screen 
+    //Method to go back to config screen for Traffic flow FROM results screen 
     public void BackToPrioritiseJuncMetricScene(){
         SceneManager.LoadScene("PrioritiseJuncMetric");
+    }
+
+    //Method to go back to results screen FROM graphic display screen 
+    public void BackToResults(){
+        SceneManager.LoadScene("ResultsScreen");
     }
 }
