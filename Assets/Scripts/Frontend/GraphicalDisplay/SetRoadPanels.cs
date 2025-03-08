0dbedf9ff55ef08986cf28fbc93842a871f265fd
Krishan Sachdev <krishansachdev@hotmail.co.uk>
2025-03-08 11:59:38 +0000

    feat(graphic display): implemented back button (NOT WORKING)

--- Assets/Scripts/Frontend/GraphicalDisplay/SetRoadPanels.cs
+++ Assets/Scripts/Frontend/GraphicalDisplay/SetRoadPanels.cs
@@ -1,29 +1,10 @@
 using System.ComponentModel;
 using UnityEngine;
+using TMPro;
 
 public class SetRoadPanels : MonoBehaviour
 {
     public GameObject parent;
-    // Panels for each combination of lanes
-    // public GameObject north2;
-    // public GameObject north3;
-    // public GameObject north4;
-    // public GameObject north5;
-
-    // public GameObject south2;
-    // public GameObject south3;
-    // public GameObject south4;
-    // public GameObject south5;
-
-    // public GameObject east2;
-    // public GameObject east3;
-    // public GameObject east4;
-    // public GameObject east5;
-
-    // public GameObject west2;
-    // public GameObject west3;
-    // public GameObject west4;
-    // public GameObject west5;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,23 +19,23 @@ public class SetRoadPanels : MonoBehaviour
         switch (StaticData.northbound.LaneCountInbound + StaticData.northbound.LaneCountOutbound)
         {
             case 2:
-                northOn = GameObject.Find("Canvas/Panel/North 2");
+                northOn = GameObject.Find("Canvas/Panel/Centre/North 2");
                 break;
             case 3:
-                northOn = GameObject.Find("Canvas/Panel/North 3");
+                northOn = GameObject.Find("Canvas/Panel/Centre/North 3");
                 break;
             case 4:
-                northOn = GameObject.Find("Canvas/Panel/North 4"); 
+                northOn = GameObject.Find("Canvas/Panel/Centre/North 4"); 
                 break;
             case 5:
-                northOn = GameObject.Find("Canvas/Panel/North 5");
+                northOn = GameObject.Find("Canvas/Panel/Centre/North 5");
                 break;
             default:
                 northOn = null;
                 Debug.LogError("Invalid panel number");
                 break;
         }
-        if (northOn != null){
+        if (northOn){
             northOn.SetActive(true);
         }
 
@@ -63,23 +44,23 @@ public class SetRoadPanels : MonoBehaviour
         switch (StaticData.southbound.LaneCountInbound + StaticData.southbound.LaneCountOutbound)
         {
             case 2:
-                southOn = GameObject.Find("Canvas/Panel/South 2");
+                southOn = GameObject.Find("Canvas/Panel/Centre/South 2");
                 break;
             case 3:
-                southOn = GameObject.Find("Canvas/Panel/South 3");
+                southOn = GameObject.Find("Canvas/Panel/Centre/South 3");
                 break;
             case 4:
-                southOn = GameObject.Find("Canvas/Panel/South 4");
+                southOn = GameObject.Find("Canvas/Panel/Centre/South 4");
                 break;
             case 5:
-                southOn = GameObject.Find("Canvas/Panel/South 5");
+                southOn = GameObject.Find("Canvas/Panel/Centre/South 5");
                 break;
             default:
                 southOn = null;
                 Debug.LogError("Invalid panel number");
                 break;
         }
-        if (southOn != null){
+        if (southOn){
             southOn.SetActive(true);
         }
 
@@ -87,23 +68,23 @@ public class SetRoadPanels : MonoBehaviour
         switch (StaticData.eastbound.LaneCountInbound + StaticData.eastbound.LaneCountOutbound)
         {
             case 2:
-                eastOn = GameObject.Find("Canvas/Panel/East 2");
+                eastOn = GameObject.Find("Canvas/Panel/Centre/East 2");
                 break;
             case 3:
-                eastOn = GameObject.Find("Canvas/Panel/East 3");
+                eastOn = GameObject.Find("Canvas/Panel/Centre/East 3");
                 break;
             case 4:
-                eastOn = GameObject.Find("Canvas/Panel/East 4");
+                eastOn = GameObject.Find("Canvas/Panel/Centre/East 4");
                 break;
             case 5:
-                eastOn = GameObject.Find("Canvas/Panel/East 5");
+                eastOn = GameObject.Find("Canvas/Panel/Centre/East 5");
                 break;
             default:
                 eastOn = null;
                 Debug.LogError("Invalid panel number");
                 break;
         }
-        if (eastOn != null){
+        if (eastOn){
             eastOn.SetActive(true);
         }
 
@@ -111,23 +92,23 @@ public class SetRoadPanels : MonoBehaviour
         switch (StaticData.westbound.LaneCountInbound + StaticData.westbound.LaneCountOutbound)
         {
             case 2:
-                westOn = GameObject.Find("Canvas/Panel/West 2");
+                westOn = GameObject.Find("Canvas/Panel/Centre/West 2");
                 break;
             case 3:
-                westOn = GameObject.Find("Canvas/Panel/West 3");
+                westOn = GameObject.Find("Canvas/Panel/Centre/West 3");
                 break;
             case 4:
-                westOn = GameObject.Find("Canvas/Panel/West 4");
+                westOn = GameObject.Find("Canvas/Panel/Centre/West 4");
                 break;
             case 5:
-                westOn = GameObject.Find("Canvas/Panel/West 5");
+                westOn = GameObject.Find("Canvas/Panel/Centre/West 5");
                 break;
             default:
                 westOn = null;
                 Debug.LogError("Invalid panel number");
                 break;
         }
-        if (westOn != null){
+        if (westOn){
             westOn.SetActive(true);
         }
     }
